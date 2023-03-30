b88995b00a5eebc89c82dbcd4f66d31825eaa3bd
ramizouari <zouari.rami@yahoo.com>
2023-03-30 03:04:23 +0200

    Fixing a minor bug in parallel generation

--- generator.py
+++ generator.py
@@ -124,19 +124,19 @@ def generate_gnp_uniform_mpg(N,P=None,C=None,a=-1,b=1,iterations=10,seed=932,loo
     if callbacks is None:
         callbacks=[]
     use_threads = threads is not None and threads > 1
-    for n in N:
-        if P is not None:
-            C = np.round(P * n).astype(int)
-        elif C is not None:
-            P = C / n
-        with ThreadPoolExecutor(max_workers=threads) as executor:
-            for c,p in zip(C,P):
-                for i in range(iterations):
-                    p=min(p,1)
-                    if use_threads:
-                            executor.submit(_generate_gnp_uniform_mpg_instance,benchmark,iteration=i,n=n,p=p,c=c,a=a,b=b,callbacks=callbacks)
-                    else:
-                        _generate_gnp_uniform_mpg_instance(benchmark,iteration=i,n=n,p=p,c=c,a=a,b=b,callbacks=callbacks)
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        for n in N:
+            if P is not None:
+                C = np.round(P * n).astype(int)
+            elif C is not None:
+                P = C / n
+                for c,p in zip(C,P):
+                    for i in range(iterations):
+                        p=min(p,1)
+                        if use_threads:
+                                executor.submit(_generate_gnp_uniform_mpg_instance,benchmark,iteration=i,n=n,p=p,c=c,a=a,b=b,callbacks=callbacks)
+                        else:
+                            _generate_gnp_uniform_mpg_instance(benchmark,iteration=i,n=n,p=p,c=c,a=a,b=b,callbacks=callbacks)
     return pd.DataFrame(benchmark)
 
 if __name__=="__main__":
